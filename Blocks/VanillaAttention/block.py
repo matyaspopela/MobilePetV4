2bdec60c2cdf3c5fde5d8d068b6187efeb3f4ab9
Matyáš Popela <popela.matyas@student.gymzr.cz>
2026-07-25 11:12:43 +0200

    Optimization of Vanilla Attention block. instead of 2x(N,N) matmul operates on 2x(N,dv)

--- Blocks/VanillaAttention/block.py
+++ Blocks/VanillaAttention/block.py
@@ -1,20 +1,29 @@
 import torch
-import torch.nn.functional as F
-import numpy as np
+from torch.autograd.function import once_differentiable
+from torch.amp import custom_fwd, custom_bwd
+
 
 class Attention(torch.autograd.Function):
+    """
+    NOTE: for readability reasons I left the weights Wq, Wk, Wv separate.
+    Although this is like 3x less efficient than W_qkv I wanted to leave it readable.
+    MultiQuery Attention block will definitely have weights glued in one Tensor.
+    """
+
     @staticmethod
+    @custom_fwd(device_type="cuda", cast_inputs=torch.bfloat16)
     def forward(ctx, input,  query_weights, key_weights, value_weights, projection_weights):
         # 1 reshape and swap dims into tokens.
         batch, c_in, height, width = input.shape
         c_w, d = query_weights.shape
+        ctx.scale = (d ** -0.5)
 
-        if c_w != c_in:
-            raise RuntimeError()
-        if not (d == key_weights.shape[1]):
-            raise RuntimeError()
-        if projection_weights.shape[1] != c_in:
-            raise RuntimeError()
+    #    if c_w != c_in:
+    #         raise RuntimeError()
+    #    if not (d == key_weights.shape[1]):
+    #         raise RuntimeError()
+    #     if projection_weights.shape[1] != c_in:
+    #         raise RuntimeError()
 
         tokenized_input = input.reshape(batch, c_in, height * width).transpose(-1,-2) # : B, N, C
 
@@ -22,7 +31,7 @@ class Attention(torch.autograd.Function):
         key_map = torch.matmul(tokenized_input, key_weights)
         value_map = torch.matmul(tokenized_input, value_weights) # N, dv
 
-        attention_map = torch.matmul(query_map, key_map.transpose(-1, -2)) * (d ** -0.5)
+        attention_map = torch.matmul(query_map, key_map.transpose(-1, -2)) * ctx.scale
         attention = torch.softmax(attention_map, dim=-1) # N, N
 
         projections_modified = torch.matmul(attention, value_map)
@@ -45,6 +54,8 @@ class Attention(torch.autograd.Function):
         return feature_map
 
     @staticmethod
+    @once_differentiable
+    @custom_bwd(device_type="cuda", cast_inputs=torch.bfloat16)
     def backward(ctx, grad_output):
 
         (query_weights,
@@ -66,20 +77,19 @@ class Attention(torch.autograd.Function):
         d_projections_modified = d_tokens_modified @ projection_weights.transpose(-1, -2)
         d_projection_weights = torch.einsum("bnd, bnc -> dc", projections_modified, d_tokens_modified)
 
-        d_attention = (d_projections_modified @ value_map.transpose(-1, -2)) * (d ** -0.5)
+        d_attention = (d_projections_modified @ value_map.transpose(-1, -2))
         d_value_map = attention.transpose(-1, -2) @ d_projections_modified
 
-        row_averages = (attention * d_attention).sum(dim=(-1), keepdim=True)
-        change = d_attention - row_averages
-        d_attention_map = attention * change
+        softmax_dot = (d_projections_modified * projections_modified).sum(-1, keepdim=True)
+        change = d_attention - softmax_dot
+        d_attention_map = attention * change * ctx.scale
 
         d_query_map = d_attention_map @ key_map
         d_key_map = d_attention_map.transpose(-1, -2) @ query_map
-        #TODO replace with clean torch.einsum
 
         d_query_weights = torch.einsum("bnc, bnd -> cd", tokenized_input, d_query_map)
         d_key_weights = torch.einsum("bnc, bnd -> cd",tokenized_input, d_key_map)
-        d_value_weights = torch.einsum("bnc, bnd -> cd", tokenized_input, d_value_map)
+        d_value_weights = torch.einsum("bnc, bnv -> cv", tokenized_input, d_value_map)
 
         d_tokenized_input_q = d_query_map @ query_weights.transpose(-1, -2)
         d_tokenized_input_k = d_key_map @ key_weights.transpose(-1, -2)
