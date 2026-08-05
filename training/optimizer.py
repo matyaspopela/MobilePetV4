3d9d7d70b630a50c276aaecd0bc0c6449e54cdd5
Matyáš Popela <popela.matyas@student.gymzr.cz>
2026-08-05 11:15:16 +0200

    Add optimizer stub.

---
+++ training/optimizer.py
@@ -0,0 +1,13 @@
+import numpy as np
+import torch
+
+class AdamW():
+    def __init__(self, params, lr, betas, eps, weight_decay):
+        pass
+
+    def zero_grad(self, set_to_none=True):
+        pass
+
+    def step(self):
+        pass
+
